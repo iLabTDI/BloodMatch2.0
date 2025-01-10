67d6ca62431fb06386725ef7c73b6ac5c15ab61a
rauf993 <raufalfonso@gmail.com>
2025-01-09 22:19:21 -0600

    carpetas

---
+++ front/backend/querys/inserts/New_User.jsx
@@ -0,0 +1,40 @@
+// USER INSERT
+import { supabase } from "../../../lib/supabase";
+
+const New_User = async ( Email,firstName,lastName,date,type,gen,password,state,city,phone,user,url) => {
+  try {
+        console.log("el email es",Email)
+        
+        const { data, error } = await supabase
+          .from('usuarios')
+          .insert([
+            {
+              Email: Email,
+              FirstName: firstName, 
+              LastName: lastName, 
+              Birthdate: date, 
+              Blood_Type: type, 
+              Sexo: gen, 
+              password:password, 
+              State: state, 
+              City: city, 
+              Phone: phone,
+              UserName: user,
+              url:url
+             
+
+            },
+          ])
+          .select();
+
+        if (error) {
+          console.error('Error al insertar datos:', error);
+        } else {
+          console.log('Datos insertados con éxito:', data);
+        }
+      } catch (error) {
+        console.error('Error al insertar datos 1:', error.message);
+      }
+    };
+
+export default New_User;
