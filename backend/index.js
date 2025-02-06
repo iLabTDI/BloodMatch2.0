8ee7bf220f1010762fa02b80ffbd8592d1aadf49
rauf993 <raufalfonso@gmail.com>
2025-02-06 01:16:36 -0600

    changes in back

--- backend/index.js
+++ backend/index.js
@@ -1,52 +1,44 @@
-const express = require("express");
-const app = express();
-const http = require("http").Server(app);
-const cors = require("cors");
+
 const { createClient } = require("@supabase/supabase-js");
 const supabase = createClient(
   "https://abgspujwyujtccknqenr.supabase.co",
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFiZ3NwdWp3eXVqdGNja25xZW5yIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTA0MzU5NzgsImV4cCI6MjAyNjAxMTk3OH0.NOjPxUVPBYztUlLCl6CBYg9vIrl9I58zD6bolUzqYfs"
 );
 
-const conexion = async (id, currentFirstName, currentSecondName) => {
+const conexion = async (id, currentFirstName, currentSecondName,currentIdSecondName,currentIdGroupName) => {
   try {
     // Crear las consultas para las dos tablas
-    const insertGroup = supabase
-      .from("savegroups2")
+
+    console.log(id,currentFirstName,currentSecondName)
+    const insertGroup = await supabase
+      .from("matches")
       .insert([
         {
-          id,
-          currentGroupName: currentFirstName,
-          currentSecondGroup: currentSecondName,
+          id_matches:id,
+          user1_id: currentFirstName,
+          user2_id: currentSecondName,
+          user1_name:currentIdGroupName,
+          user2_name:currentIdSecondName,
+
         },
       ]);
 
-    const insertMessage = supabase.from("messages").insert([{ idGroup: id }]);
+    // const insertMessage = supabase.from("messages").insert([{ idGroup: id }]);
 
-    // Ejecutar las consultas en paralelo
-    const [groupResult, messageResult] = await Promise.all([
+    // // Ejecutar las consultas en paralelo
+    const [groupResult] = await Promise.all([
       insertGroup,
-      insertMessage,
+      //insertMessage,
     ]);
 
     // Manejar los resultados
     if (groupResult.error) {
       console.log("Error al insertar en savegroups2:", groupResult.error);
     } else {
-      console.log(
-        "Datos insertados correctamente en savegroups2:",
-        groupResult.data
-      );
+      console.log("Datos insertados correctamente en savegroups2:", groupResult.data);
     }
 
-    if (messageResult.error) {
-      console.log("Error al insertar en messages:", messageResult.error);
-    } else {
-      console.log(
-        "Datos insertados correctamente en messages:",
-        messageResult.data
-      );
-    }
+  
   } catch (error) {
     console.error("Error durante la operación:", error);
   }
@@ -57,10 +49,7 @@ const verify = (currentGroupName, currentSecondGroup) => {
   console.log("la longitud es", longitud);
 
   for (let i = 0; i < chatgroups.length; i++) {
-    console.log(
-      chatgroups[i].currentGroupName,
-      chatgroups[i].currentSecondGroup
-    );
+    
     if (
       (currentGroupName === chatgroups[i].currentGroupName ||
         currentGroupName === chatgroups[i].currentSecondGroup) &&
@@ -76,78 +65,204 @@ const verify = (currentGroupName, currentSecondGroup) => {
     }
   }
 };
-const socketIO = require("socket.io")(http, {
-  cors: {
-    origin: "*", // Permitir todas las conexiones
-  },
-});
 
+
+const express = require("express");
+const app = express();
+const http = require("http").Server(app);
+const cors = require("cors");
 const fs = require("fs");
 const path = require("path");
 const PORT = 8002;
-
+//allow all connections 
+const socketIO = require("socket.io")(http, {
+  cors: {
+    origin: "*", 
+  },
+});
 function createUniqueId() {
   return Math.random().toString(20).substring(2, 10);
 }
 
 let chatgroups = [];
+let current_id=0;
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cors());
+socketIO.on("connection", (socket) => {
+  console.log(`${socket.id} user is just connected`);
+  //console.log(" lo que hay en el grupo es ",chatgroups)
+  socket.on("getAllGroups", () => {
+    socket.emit("groupList", chatgroups);
+   // console.log("el socket es", chatgroups);
+  });
+  socket.on("createNewGroup",async ({ currentGroupName, currentSecondGroup ,currentIdSecondName,currentIdGroupName}) => {
+      try {
+       current_id++
+        const newGroup = {
+          id:current_id,
+          currentIdGroupName,
+          currentGroupName,
+          currentIdSecondName,
+          currentSecondGroup,
+          messages: [],
+        };
+
+        if (verify(currentGroupName, currentSecondGroup) > 0) {
+          console.log("no va a hacer nada ejje");
+        } else {
+          await conexion(current_id, currentGroupName, currentSecondGroup,currentIdGroupName,currentIdSecondName);
+
+          // i will print the new group
+          console.log("Nuevo grupo creado:", newGroup);
+
+          // add new group to my chat list
+          chatgroups.unshift(newGroup);
+          //console.log(chatgroups)
+          socketIO.emit("groupList", chatgroups);
+
+           
+          //saveChatGroups();
+       }
+      } catch (e) {
+        //console.log("el error es", e);
+      }
+    }
+  );      
+  
+
+  socket.on("findGroup", (id) => {
+    const numericId = Number(id);
+    console.log("el id del gruop que llega es",numericId)
+    const filteredGroup = chatgroups.filter((item) => item.id === numericId);
+
+    if (filteredGroup.length > 0 && filteredGroup[0].messages) {
+      // console.log("El grupo filtrado es:", filteredGroup);
+     
+      socket.emit("foundGroup", filteredGroup[0].messages);
+    } else {
+      console.log("No se encontró el grupo o no tiene mensajes.");
+      socket.emit("foundGroup", []);
+    }
+  });
+  socket.on("encontrar", (id) => {
+    const numericId = Number(id);
+    //console.log("ID recibido en el servidor:", numericId); // Log para verificar el ID
+    const filteredGroup2 = chatgroups.filter((item) => item.id === numericId);
+
+    //console.log("Grupo filtrado:", filteredGroup2);
+
+    socket.emit("found", filteredGroup2);
+  });
+
+ 
+  socket.on("newChatMessage", async (data) => {
+    const { currentChatMesage, groupIdentifier, currentUser, timeData } = data;
+    const filteredGroup = chatgroups.filter(
+      (item) => item.id === groupIdentifier
+    );
+
+    console.log("el mesaje",groupIdentifier)
+
+    const newMessage = {
+      id: createUniqueId(),
+      text: currentChatMesage,
+      currentUser,
+      time: `${timeData.hr}:${timeData.mins}`,
+    };
+
+
+    // Ensure the group exists
+    if (filteredGroup.length > 0) {
+      console.log("elgrupo filtrado esf", filteredGroup);
+      if (!Array.isArray(filteredGroup[0].messages)) {
+        filteredGroup[0].messages = [];
+      }
+      filteredGroup[0].messages.push(newMessage);
+
+      const { data, error } = await supabase
+        .from("matches")
+        .update([{ messages: filteredGroup[0].messages }])
+        .eq("id_matches", groupIdentifier);
+      if (error) {
+        console.log(error);
+      } else {
+        console.log(data);
+      }
+
+      // Emit the new message to all clients in the group room
+      socket
+        .to(filteredGroup[0].currentGroupName)
+        .emit("groupMessage", newMessage);
+
+        console.log("filteredGroup[0].messages",filteredGroup[0].messages)
+
+      // Emit the updated list of groups and the messages of the found group to the client that sent the message
+      socket.emit("groupList", chatgroups);
+      socket.emit("foundGroup", filteredGroup[0].messages);
+
+      // Save the updated chat groups
+      //saveChatGroups();
+    }
+  });
+});
+
 
 // Cargar grupos de chat desde el archivo JSON cuando se inicia el servidor
 async function loadChatGroups() {
   try {
     const { data: groupsData, error: groupsError } = await supabase
-      .from("savegroups2")
+      .from("matches")
       .select("*");
-    const { data: messagesData, error: messagesError } = await supabase
-      .from("messages")
-      .select("*");
-
+   
     if (groupsError) {
       console.log("Error loading groups:", groupsError);
       return;
     }
-    if (messagesError) {
-      console.log("Error loading messages:", messagesError);
-      return;
-    }
+   
+    if (groupsData.length > 0) {   
+      //console.log("Groups data:", groupsData);
+      current_id = Math.max(...groupsData.map(group => group.id_matches), 0);
+      //console.log("Starting ID:", current_id);
 
-    console.log("Groups data:", groupsData);
-    console.log("Messages data:", messagesData);
-
-    if (groupsData.length > 0) {
       chatgroups = groupsData.map((group) => {
-        const groupMessages = messagesData.find(
-          (message) => message.idGroup === group.id
+        const groupMessages = groupsData.find(
+          (message) => message.id_matches === group.id_matches
         );
 
-        let messages = [];
+        
+         let messages = [];
         if (groupMessages && Array.isArray(groupMessages.messages)) {
           messages =
             groupMessages.messages.length === 1
               ? groupMessages.messages[0]
               : groupMessages.messages;
         }
+        //console.log("mesages 2",messages)
 
         return {
-          id: group.id,
-          currentGroupName: group.currentGroupName,
-          currentSecondGroup: group.currentSecondGroup,
-          messages: messages,
+          id: group.id_matches,
+          currentGroupName: group.user1_id,
+          currentSecondGroup: group.user2_id,
+          currentIdGroupName:group.user2_name,
+          currentIdSecondName:group.user1_name,
+          messages: group.messages,
         };
       });
 
-      console.log("Grupos actualizados correctamente:", chatgroups);
+      //console.log("Grupos actualizados correctamente:", chatgroups);
     } else {
       console.log("No hay grupos");
+      current_id = 0;
     }
   } catch (e) {
     console.log("An error occurred:", e);
   }
 }
 
-// Cargar los datos cuando el servidor se inicie
+
 loadChatGroups().then(() => {
-  console.log("Los grupos del log son", chatgroups);
+  //console.log("Los grupos del log son", chatgroups);
 });
 
 //}
@@ -176,124 +291,13 @@ async function saveChatGroups() {
 // Load the data when the server starts
 (async () => {
   await loadChatGroups();
-  console.log("Los grupos del log son", chatgroups);
+
 })();
 
-console.log("los grupos del log es", chatgroups);
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-app.use(cors());
+
+
 
 //in this part the user is connected to the server
-socketIO.on("connection", (socket) => {
-  console.log(`${socket.id} user is just connected`);
-
-  socket.on("getAllGroups", () => {
-    socket.emit("groupList", chatgroups);
-    console.log("el socket es", chatgroups);
-  });
-
-  socket.on(
-    "createNewGroup",
-    async ({ currentGroupName, currentSecondGroup }) => {
-      try {
-        const newGroup = {
-          id: chatgroups.length + 1,
-          currentGroupName,
-          currentSecondGroup,
-          messages: [],
-        };
-
-        var id = chatgroups.length + 1;
-
-        if (verify(currentGroupName, currentSecondGroup) > 0) {
-          console.log("no va a hacer nada ejje");
-        } else {
-          await conexion(id, currentGroupName, currentSecondGroup);
-
-          // i will print the new group
-          console.log("Nuevo grupo creado:", newGroup);
-
-          // add new group to my chat list
-          chatgroups.unshift(newGroup);
-
-          socketIO.emit("groupList", chatgroups);
-          //saveChatGroups();
-        }
-      } catch (e) {
-        console.log("el error es", e);
-      }
-    }
-  );
-
-  socket.on("findGroup", (id) => {
-    const numericId = Number(id);
-    const filteredGroup = chatgroups.filter((item) => item.id === numericId);
-
-    if (filteredGroup.length > 0 && filteredGroup[0].messages) {
-      console.log("El grupo filtrado es:", filteredGroup);
-      socket.emit("foundGroup", filteredGroup[0].messages);
-    } else {
-      console.log("No se encontró el grupo o no tiene mensajes.");
-      socket.emit("foundGroup", []);
-    }
-  });
-
-  socket.on("encontrar", (id) => {
-    const numericId = Number(id);
-    //console.log("ID recibido en el servidor:", numericId); // Log para verificar el ID
-    const filteredGroup2 = chatgroups.filter((item) => item.id === numericId);
-
-    //console.log("Grupo filtrado:", filteredGroup2);
-    socket.emit("found", filteredGroup2);
-  });
-
-  socket.on("newChatMessage", async (data) => {
-    const { currentChatMesage, groupIdentifier, currentUser, timeData } = data;
-    const filteredGroup = chatgroups.filter(
-      (item) => item.id === groupIdentifier
-    );
-
-    const newMessage = {
-      id: createUniqueId(),
-      text: currentChatMesage,
-      currentUser,
-      time: `${timeData.hr}:${timeData.mins}`,
-    };
-
-    // Ensure the group exists
-    if (filteredGroup.length > 0) {
-      console.log("elgrupo filtrado esf", filteredGroup);
-      if (!Array.isArray(filteredGroup[0].messages)) {
-        filteredGroup[0].messages = [];
-      }
-      filteredGroup[0].messages.push(newMessage);
-      // await sendMessages(groupIdentifier,newMessage)
-
-      const { data, error } = await supabase
-        .from("messages")
-        .update([{ messages: filteredGroup[0].messages }])
-        .eq("idGroup", groupIdentifier);
-      if (error) {
-        console.log(error);
-      } else {
-        console.log(data);
-      }
-
-      // Emit the new message to all clients in the group room
-      socket
-        .to(filteredGroup[0].currentGroupName)
-        .emit("groupMessage", newMessage);
-
-      // Emit the updated list of groups and the messages of the found group to the client that sent the message
-      socket.emit("groupList", chatgroups);
-      socket.emit("foundGroup", filteredGroup[0].messages);
-
-      // Save the updated chat groups
-      //saveChatGroups();
-    }
-  });
-});
 
 app.get("/api", (req, res) => {
   res.json(chatgroups);
