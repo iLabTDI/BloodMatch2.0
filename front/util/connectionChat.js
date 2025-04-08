1bb8630e50912ef6ba692e8af6a907277da1a572
Manuel Alejandro Rodriguez Sanchez <manuel.rs003@gmail.com>
2025-04-08 12:48:11 -0600

    APK falta el mapa

--- front/util/connectionChat.js
+++ front/util/connectionChat.js
@@ -5,5 +5,6 @@ export const BaseUrl =
     ? "http://10.214.80.136:3000/"
     : "http://localhost:3000";
 
-export const socket = io.connect("http://10.214.109.19:8002/");
+// export const socket = io.connect("http://10.214.96.135:8002/");
+export const socket = io.connect("https://backend-bloodmatch.onrender.com");
 // export const socket = io.connect("https://vibrant-dawn-08238.pktriot.net/")
