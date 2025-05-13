f71ecd418cbf65781b618335d2bc763ad960840d
Manuel Alejandro Rodriguez Sanchez <manuel.rs003@gmail.com>
2025-05-13 11:02:19 -0600

    Mantener sesion iniciada

--- front/util/connectionChat.js
+++ front/util/connectionChat.js
@@ -5,5 +5,5 @@ export const BaseUrl =
         ? "http://10.214.80.136:3000/"
         : "http://localhost:3000";
 
-export const socket = io.connect("http://10.214.79.11:8002/");
-// export const socket = io.connect("https://backend-bloodmatch-app.onrender.com");
+// export const socket = io.connect("http://10.214.79.11:8002/");
+export const socket = io.connect("https://backend-bloodmatch-app.onrender.com");
