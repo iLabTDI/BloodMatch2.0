4569843c959c8e4c550dd7a6a7d3a2aa05db4f53
cesar jan karlo <leossuarez103@gmail.com>
2025-06-19 17:34:19 -0600

    se agregaron las rutas de imagenes

--- backend/src/routes/img.ts
+++ backend/src/routes/img.ts
@@ -2,4 +2,23 @@ import { Router } from "express";
 
 const router = Router()
 
+//       ==rutas de imagenes==
+
+//Esta ruta se encarga de crear imagenes
+router.post("/");
+
+//Esta ruta se encarga de optener un imagen
+router.get("/",(req, res)=>{
+    res.send({data: "Aqui van los modelos"})
+});
+
+//Esta ruta optiene todas las imagenes
+router.get("/");
+
+//Esta ruta se encarga de editar la imagen
+router.put("/");
+
+//Esta ruta se encarga de eliminar imagene
+router.delete("/");
+
 export default router;
