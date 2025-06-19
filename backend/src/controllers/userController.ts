082ccee304d19af5319feee5fba9da1178e97fa6
cesar jan karlo <leossuarez103@gmail.com>
2025-06-19 17:55:43 -0600

    se agrego el controlador de usuarios

---
+++ backend/src/controllers/userController.ts
@@ -0,0 +1,49 @@
+import {Request, Response} from "express";
+import { handleHttp } from "../utils/error.handle";
+
+//controller para obtener un usuario
+const getUser = ({ body }:Request, res: Response) => {
+        try {
+                res.send(body);
+        } catch (e) {
+        handleHttp(res, 'ERROR_GET_USER')
+        }
+}
+//controller para obtener un usuarios
+const getUsers = ({ body }:Request, res: Response) => {
+        try{
+                res.send(body)
+        } catch (e) {
+                handleHttp(res, 'ERROR_GET_USERS')
+        }
+}
+
+//controller para actualizar usuario
+const updateUser = ({ body }:Request, res: Response) => {
+        try{
+                res.send(body)
+        }catch (e) {
+                handleHttp(res, 'ERROR_UPDATE_USER')
+        }
+}
+
+//controller para inserta un usuario
+const postUser = ({ body }:Request, res: Response) => {
+        try{
+                res.send(body);
+        } catch (e) {
+                handleHttp(res, 'ERROR_INSERT_USER')
+        }
+}
+
+//controller para eliminar un usuario
+const deleUser = ({body}:Request, res: Response) => {
+        try{
+                res.send(body)
+        } catch (e) {
+                handleHttp(res, 'ERROR_DELET_USER')
+        }
+}
+
+//exportamos las controller
+export {getUser, getUsers, updateUser, postUser, deleUser };
