aea8729a80490a186221d35bbf3ddf8b22f5e117
cesar jan karlo <leossuarez103@gmail.com>
2025-06-20 13:50:27 -0600

    se agregaron los controllers a la ruta img

--- backend/src/routes/img.ts
+++ backend/src/routes/img.ts
@@ -1,24 +1,24 @@
 import { Router } from "express";
+import { deleImg, getImg, getImgs, postImg, updateImg } from "../controllers/imgController";
 
 const router = Router()
 
 //       ==rutas de imagenes==
 
-//Esta ruta se encarga de crear imagenes
-router.post("/");
+
+//para inserta un img tienes que tener un id(usuario registrado)
+router.post("/id:", postImg);//Esta ruta se encarga de inserta imagenes
 
 //Esta ruta se encarga de optener un imagen
-router.get("/",(req, res)=>{
-    res.send({data: "Aqui van los modelos"})
-});
+router.get("/id:", getImg);
 
 //Esta ruta optiene todas las imagenes
-router.get("/");
+router.get("/", getImgs);
 
 //Esta ruta se encarga de editar la imagen
-router.put("/");
+router.put("/id", updateImg);
 
 //Esta ruta se encarga de eliminar imagene
-router.delete("/");
+router.delete("/id", deleImg);
 
 export default router;
