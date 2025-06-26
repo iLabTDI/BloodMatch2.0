cae84e3174c2ede5ea950e40446b15d384ceaba0
cesar jan karlo <leossuarez103@gmail.com>
2025-06-26 15:42:47 -0600

    se agrego Manejador de errores de BD(models)

--- backend/src/utils/error.handle.ts
+++ backend/src/utils/error.handle.ts
@@ -1,7 +1,8 @@
 import { Response } from "express"
 import { CustomError } from "./CustomError";
+
 //Manejador de errores de las respuestas HTTP(controlls)
-const handleHttp = (res: Response, error: unknown) => {
+export const handleHttp = (res: Response, error: unknown) => {
     if(error instanceof CustomError){
         console.error(`⚠️ [${error.code}]`, error.message);
         res.status(error.statusCode).json({
@@ -20,4 +21,21 @@ const handleHttp = (res: Response, error: unknown) => {
     }
 };
 
-export {handleHttp};
+//Manejador de errores de BD(models)
+export const throwModelError =(
+    message: string,
+    error: unknown, 
+    code: string = "UNHANDLE_MODEL_ERROR", 
+    statusCode:number = 500) =>{
+    //variables para mensaje y details
+    let fullMessage = message;
+    let details = null;
+
+    if(error && typeof error === "object" && "message" in error){
+        fullMessage += `: ${(error as any).message}`;
+        details = error;
+    }
+
+    throw new CustomError(fullMessage, code, statusCode, details);
+};
+
