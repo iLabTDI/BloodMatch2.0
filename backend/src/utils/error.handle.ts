bcced762aa6e271da03b4820792ccf80ae60d805
cesar jan karlo <leossuarez103@gmail.com>
2025-06-26 15:29:48 -0600

    se modifico el manejador de errores de HTTP(se agrego customError)

--- backend/src/utils/error.handle.ts
+++ backend/src/utils/error.handle.ts
@@ -1,9 +1,23 @@
 import { Response } from "express"
-
-//Manejador de errores de las respuestas HTTP
-const handleHttp = (res: Response, error: string) => {
-    res.status(500)
-    res.send({error})
+import { CustomError } from "./CustomError";
+//Manejador de errores de las respuestas HTTP(controlls)
+const handleHttp = (res: Response, error: unknown) => {
+    if(error instanceof CustomError){
+        console.error(`⚠️ [${error.code}]`, error.message);
+        res.status(error.statusCode).json({
+            error: {
+                message: error.message,
+                code: error.code,
+                details: error.details || null,
+            },
+        });
+    } else if (error instanceof Error){
+        console.error("❌ Error desconocido:", error.message);
+        res.status(500).json({error: {message: error.message, code: "UNHANDLED_ERROR"}});
+    }else{
+        console.error("❌ Error inesperado:", error);
+        res.status(500).json({error: {message: "Error desconocido", code: "UNKNOWN"}});
+    }
 };
 
 export {handleHttp};
