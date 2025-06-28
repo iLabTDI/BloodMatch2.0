6b7655479f2fbd5fbd3c446d61159759d91aaed2
cesar jan karlo <leossuarez103@gmail.com>
2025-06-28 14:38:25 -0600

    se cambio el nombre de 3 rutas de user

--- backend/src/routes/user.ts
+++ backend/src/routes/user.ts
@@ -1,18 +1,18 @@
 import { Router } from "express";
-import { deleUser, getUser,postLogin, postlogout, postRegister, updateUser } from "../controllers/userController";
+import { deleUser, getUser, loginUser, logoutUser, registerUser, updateUser } from "../controllers/userController";
 
 const router = Router()
 
 //       ==rutas de usuario==
 
 //Esta ruta se encarga de hacer loging un usuario
-router.post("/login", postLogin);
+router.post("/login", loginUser);
 
 //esta ruta de encarga de registra un usuario
-router.post("/register", postRegister);
+router.post("/register", registerUser);
 
 //esta ruta se encarga de cerrar la sesion de usuario
-router.post("/logout", postlogout);
+router.post("/logout", logoutUser);
 
 //Esta ruta se encarga de optener un usuario
 router.get("/:id", getUser);
