540970d87a4e7407e1df2f2ede7507e9a24c5196
cesar jan karlo <leossuarez103@gmail.com>
2025-07-03 16:12:31 -0600

    Controlls de login y register de auth

---
+++ backend/src/controllers/auth.ts
@@ -0,0 +1,25 @@
+import {Request, Response} from "express";
+import { handleHttp } from "../utils/error.handle";
+import * as UserService from "../service/auth"
+
+
+//controller para registra un usuario
+export const registerUser = async (req: Request, res: Response) => {
+        try{
+                console.log("req.body:", req.body); // 🔍 Verifica aquí
+                const newUser = await UserService.registerUser(req.body); //se implementa logica de service
+                res.status(201).json({message: "Usuario registrado exitosamente", user: newUser});
+        } catch (e: any) {
+                handleHttp(res, e)
+        }
+}
+
+//controller para hacer login
+export const loginUser = async (req: Request, res: Response) => {
+        try{
+                const User = await UserService.loginUser(req.body);
+                res.status(201).json({message: "Login exitoso",user: User});
+        } catch (e) {
+                handleHttp(res, e)
+        }
+}
