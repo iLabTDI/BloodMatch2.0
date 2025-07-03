5b8a8e549787c68b5a866351822bf84b59866eda
cesar jan karlo <leossuarez103@gmail.com>
2025-07-03 15:42:38 -0600

    se elimininaron las rutas login y register

--- backend/src/routes/user.ts
+++ backend/src/routes/user.ts
@@ -1,15 +1,9 @@
 import { Router } from "express";
-import { deleUser, getUser, loginUser, logoutUser, registerUser, updateUser } from "../controllers/userController";
+import { deleUser, getUser, logoutUser, updateUser } from "../controllers/userController";
 
 const router = Router()
 
 //       ==rutas de usuario==
-
-//Esta ruta se encarga de hacer loging un usuario
-router.post("/login", loginUser);
-
-//esta ruta de encarga de registra un usuario
-router.post("/register", registerUser);
 
 //esta ruta se encarga de cerrar la sesion de usuario
 router.post("/logout", logoutUser);
