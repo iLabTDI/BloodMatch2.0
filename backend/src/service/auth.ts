0f61e2ba61c651c858418b2fa222839d31776acd
cesar jan karlo <leossuarez103@gmail.com>
2025-11-15 22:59:37 -0600

    se refactorizo en dos user y auth

--- backend/src/service/auth.ts
+++ backend/src/service/auth.ts
@@ -1,25 +1,20 @@
+//Se encarga de registra User
 import { User } from "../interface/user";
 import * as UserModel from "../models/user";
-import bcrypt from "bcryptjs"
 import { CustomError } from "../utils/CustomError";
+import { encrypt, verified } from "../utils/crypt.handle";
 
-//Se encarga de registra User
 export const registerUser = async (user: User) => {
-    //validacion basica
+    
     if (!user.Email) throw new CustomError("El correo es obligatorio", "USER_EMAIL_REQUIRED", 400);
     if (!user.Password) throw new CustomError("La contraseña es obligatoria", "USER_PASWORD_REQUIRED", 400);
 
-    //validar que no exita el correo antes
-    //const existing = await UserModel.getUserByEmail(user.Email);
-    //if (existing)  throw new CustomError("El usuario ya existe", "USER_ALREADY_EXISTS", 409);
+    //agregar validacion extra(zod) o express validetor
 
-    //agregar validacion extra(zod)
-
-    //hash de contraseña(bcrypt)
-    const hashedPassword = await bcrypt.hash(user.Password, 10);
+    const passhash = await encrypt(user.Password)
     const newUser: User = {
         ...user,
-        Password: hashedPassword
+        Password: passhash
     };
 
     //se agrega usuario a models(BD)
@@ -28,10 +23,17 @@ export const registerUser = async (user: User) => {
 };
 
 //se encarga de hacer login
-export const loginUser = async (id: string) => {
-    const user = await UserModel.getUserById(id);
-    //validacion basica
+export const loginUser = async (email: string, password: string) => {
+    //Obtenemos el usuario por medio del email
+    const user = await UserModel.getUserByEmail(email);
+    //valibasicadacion 
     if(!user) throw new CustomError("Usuario no encontrado","USER_NOT_FOUND",404);
+    if(!email) throw new CustomError("El correo es obligatorio","USER_EMAIL_REQUIRED",404);
+    if(!password) throw new CustomError("La contraseña es obligatoria","USER_PASSWORD_REQUIRED",404);
+
+    //Validamos la contraseña
+    const isPasswordValid = await verified(password, user.Password);
+    if(!isPasswordValid) throw new CustomError("Contraseña incorrecta", "INVALID_PASSWORD", 403)
 
     //hacer mas robusta la validacion de usuario
     return user;
