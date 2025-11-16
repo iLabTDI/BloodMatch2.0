e5c8bf4bc3737c0647040c5184691d42322bfc02
cesar jan karlo <leossuarez103@gmail.com>
2025-11-15 22:58:55 -0600

    se refactorizo en dos user y auth

--- backend/src/routes/auth.ts
+++ backend/src/routes/auth.ts
@@ -3,12 +3,10 @@ import {loginUser, registerUser} from "../controllers/auth";
 
 const router = Router()
 
-//       ==rutas de usuario(auth)==
+//rutas de usuario(auth)
 
-//Esta ruta se encarga de hacer loging un usuario
 router.post("/login", loginUser);
 
-//esta ruta de encarga de registra un usuario
 router.post("/register", registerUser);
 
 export default router;
--- backend/src/routes/user.ts
+++ backend/src/routes/user.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { deleUser, getUser, logoutUser, updateUser } from "../controllers/userController";
+import { deleUser, logoutUser, updateUser } from "../controllers/userController";
 
 const router = Router()
 
@@ -7,9 +7,6 @@ const router = Router()
 
 //esta ruta se encarga de cerrar la sesion de usuario
 router.post("/logout", logoutUser);
-
-//Esta ruta se encarga de optener un usuario
-router.get("/:id", getUser);
 
 //Esta ruta se encarga de editar un usuario
 router.put("/:id", updateUser);
