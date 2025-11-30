17e4f79d395ceac91c43cdd505f0ee4a8c73bca3
cesar jan karlo <leossuarez103@gmail.com>
2025-11-30 16:45:04 -0600

    se modifico

--- backend/src/routes/auth.ts
+++ backend/src/routes/auth.ts
@@ -1,12 +1,11 @@
 import { Router } from "express";
 import {loginUser, registerUser} from "../controllers/auth";
-
 const router = Router()
 
 //rutas de usuario(auth)
 
-router.post("/login", loginUser);
+router.post("/login",loginUser);
 
-router.post("/register", registerUser);
+router.post("/register",registerUser);
 
 export default router;
