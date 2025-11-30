948c583cd5c4a4a746125b28c9de6fb8d1484223
cesar jan karlo <leossuarez103@gmail.com>
2025-11-30 16:39:37 -0600

    se encarga de generar jwt

---
+++ backend/src/utils/jwt.handle.ts
@@ -0,0 +1,13 @@
+import jwt from "jsonwebtoken";
+
+const JWT_SECRET = process.env.JWT_SECRET || "BloatMatchkeyA+420";
+
+export const generateToken = (email: string) => {
+    return jwt.sign({ email }, JWT_SECRET, {
+        expiresIn: "2h",
+    });
+};
+
+export const verifyToken = () => {
+    
+};
