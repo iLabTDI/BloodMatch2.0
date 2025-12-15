dd3e565d16c9cbf8ea982e6aa028a19f9d345f23
cesar jan karlo <leossuarez103@gmail.com>
2025-12-15 16:36:10 -0600

    se modifico

--- backend/src/routes/auth.ts
+++ backend/src/routes/auth.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import {loginUser, registerUser} from "../controllers/auth";
+import {loginUser, registerUser} from "../controllers/User/auth";
 const router = Router()
 
 //rutas de usuario(auth)
