a9662dff2152273708c9780020620ad414108685
cesar jan karlo <leossuarez103@gmail.com>
2025-12-15 16:44:30 -0600

    se cambio a la capa User

---
+++ backend/src/service/User/user.ts
@@ -0,0 +1,20 @@
+import { User } from "../../interface/User/user";
+import * as UserModel from "../../models/user";
+import bcrypt from "bcryptjs"
+
+//se encarga de Actualizar User
+export const updateUser = async (id: string, userData: Partial<User>) => {
+    //Si se actualiza la contraseña se hashea
+    if(userData.Password){
+        userData.Password = await bcrypt.hash(userData.Password, 10)
+    }
+
+    const result =await UserModel.updateUser(id, userData);
+    return result;
+}
+
+//se encarga de delate user
+export const deleteUser = async (id: string) => {
+    const result = await UserModel.deleteUser(id);
+    return result;
+}
