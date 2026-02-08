9d89ed1f0743a5b85f495b0439bf5fc9c80b37dd
cesar jan karlo <leossuarez103@gmail.com>
2026-02-08 15:32:32 -0600

    se modifico

--- backend/src/utils/jwt.handle.ts
+++ backend/src/utils/jwt.handle.ts
@@ -1,15 +1,19 @@
 //Utils for handling JWT tokens
-import jwt from "jsonwebtoken";
+import jwt,{JwtPayload} from "jsonwebtoken";
 
 const JWT_SECRET = process.env.JWT_SECRET || "BloatMatchkeyA+420";
 
-export const generateToken = (email: string) => {
-    return jwt.sign({ email }, JWT_SECRET, {
-        expiresIn: "2h",
+interface TokenPayload {
+    id: number;
+    email: string;
+}
+
+export const generateToken = (payload: TokenPayload): string => {
+    return jwt.sign( payload, JWT_SECRET, {
+            expiresIn: "2h",
     });
 };
 
-export const verifyToken = (token : string) => {
-    const isOk = jwt.verify(token, JWT_SECRET);
-    return isOk;
+export const verifyToken = (token : string): TokenPayload | JwtPayload => {
+    return jwt.verify(token, JWT_SECRET) as TokenPayload;
 };
